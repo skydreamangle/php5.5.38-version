afa100a59bfe7ac4d11a9f5dfeceeeafccfca8e6
skydreamangle <35449045+skydreamangle@users.noreply.github.com>
2018-01-18 14:48:59 +0800

    Update entrypoint.sh

--- manifest/entrypoint.sh
+++ manifest/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p /var/www/html/gamma/daf/tmp/cache
-mkdir -p /var/www/html/gamma/daf/tmp/html
-chmod 777 -R /var/www/html/gamma/daf/tmp
+mkdir -p /var/www/html/yoursourcecodefoldername/tmp/cache
+mkdir -p /var/www/html/yoursourcecodefoldername/tmp/html
+chmod 777 -R /var/www/html/yoursourcecodefoldername/tmp
 
 service redis_6379 start
 exec "$@"
